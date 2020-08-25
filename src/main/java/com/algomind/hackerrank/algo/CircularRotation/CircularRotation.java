cef1dd86b528012f5ffe5b786df87e2731ac63c7
Kefeng Zhang <kefeng.zhang@algomind.co.uk>
2020-08-25 23:37:39 +0100

    Added CircularArrayRotation

---
+++ src/main/java/com/algomind/hackerrank/algo/CircularRotation/CircularRotation.java
@@ -0,0 +1,16 @@
+package com.algomind.hackerrank.algo.CircularRotation;
+
+import java.util.Arrays;
+
+public class CircularRotation {
+    int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        int numberOfRotation = k % a.length;
+        return Arrays.stream(queries).map(i -> a[oldIndex(i, numberOfRotation, a.length)]).toArray();
+    }
+
+    private int oldIndex(int index, int numberOfRotation, int length) {
+        int oldIndex = index - numberOfRotation;
+        if(oldIndex < 0) oldIndex = oldIndex + length;
+        return oldIndex;
+    }
+}
---
+++ src/test/java/com/algomind/hackerrank/algo/CircularRotation/CircularRotationTest.java
@@ -0,0 +1,24 @@
+package com.algomind.hackerrank.algo.CircularRotation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class CircularRotationTest {
+    private CircularRotation circularRotation = new CircularRotation();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int[] results, int[] a, int k, int[] queries) {
+        Assertions.assertArrayEquals(results, circularRotation.circularArrayRotation(a, k, queries));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(new int[]{2,3,1}, new int[]{1,2,3}, 2, new int[]{0, 1, 2})
+        );
+    }
+}
